a36f2d30d24569c968452e9771e9e680f0e9f037
Viktor <167817135+vlovold@users.noreply.github.com>
2026-03-27 12:13:12 +0100

    Created general structure and imported libraries

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import scipy
+import matplotlib as mpl
+import numpy
+
+#Collect CSV file and store it in data structure
+
+#Preprocessing
+#Find nan values and deal with them
+
+#Processing
+#Normalize values, minmax, z-score, robust scaling
+
+#Analysis
+#Pearson correlation, Linear disciminant analysis (PCA, LDA),
+#Multi layer perceptron
+
