72c9441f523f36e7fbebc207d345c29b76e59def
Viktor <167817135+vlovold@users.noreply.github.com>
2026-03-30 11:14:21 +0200

    Added pearsonr correlation with matrices

--- src/models.py
+++ src/models.py
@@ -5,6 +5,11 @@ import pandas
 #Pearson correlation, Linear disciminant analysis (PCA, LDA)
 
 def pearson_corr(dataframe, column, correlation):
-    pear = dataframe[column].corr(dataframe[correlation])
-    return pear
+    matrix = []
+    for i in range(4):
+        row = []
+        for j in range(4):
+            row.append(stats.pearsonr(dataframe.iloc[i], dataframe.iloc[j]))
+    r, p = stats.pearsonr(dataframe[column], dataframe[correlation])
+    return r
 #Multi layer perceptron
